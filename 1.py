0fc9feaa424ac7c79a50b02c9d85dd15fd73bee9
ykyyg <63894609+ykyyg@users.noreply.github.com>
2020-05-17 10:03:34 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -78,7 +78,7 @@ def main():
     except:
         print("pass")
         pass
-for _ in range(60): 
+for _ in range(10): 
     for i in range(random.randint(600,1200),0,-1):
         time.sleep(1)
     main()
